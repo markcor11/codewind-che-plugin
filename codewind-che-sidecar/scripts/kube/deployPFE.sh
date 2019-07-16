115392eda41ca1d66c0d115c1ed5c42082274e4c
mark-cornaia <mark.cornaia@uk.ibm.com>
2019-07-16 11:20:15 +0100

    Change method of determining hostname
    Signed-off-by: mark-cornaia <mark.cornaia@uk.ibm.com>

--- codewind-che-sidecar/scripts/kube/deployPFE.sh
+++ codewind-che-sidecar/scripts/kube/deployPFE.sh
@@ -36,13 +36,10 @@ setTemplateValue $DEPLOY_DIR/codewind.yaml KUBE_NAMESPACE_PLACEHOLDER $NAMESPACE
 # Set the owner the Codewind deployment and service
 setCodewindOwner
 
-# Get the che ingress host
-CHE_INGRESS_HOST=$(kubectl get ingress -n che che-ingress -o jsonpath="{.spec.rules[0].host}")
-sed -i "s/CHE_INGRESS_HOST_PLACEHOLDER/$CHE_INGRESS_HOST/g" /scripts/kube/codewind_template.yaml
-
-# Get the che ingress domain
-CHE_INGRESS_DOMAIN=$(echo $CHE_INGRESS_HOST | sed -e "s/che-che.//g")
-sed -i "s/CHE_INGRESS_DOMAIN_PLACEHOLDER/$CHE_INGRESS_DOMAIN/g" /scripts/kube/codewind_template.yaml
+# Get the CHE_API url and extract the hostname component
+CHE_INGRESS_HOST=$(echo $CHE_API | sed -e 's#http[s]*://##') # remove protocol
+CHE_INGRESS_HOST=$(echo $CHE_INGRESS_HOST | sed -e 's#/.*##') # remove path
+sed -i "s/CHE_INGRESS_HOST_PLACEHOLDER/$CHE_INGRESS_HOST/g" /scripts/kube/codewind_template.yaml 
 
 # Check if we're on IBM Cloud Private and if so, apply the ibm-privileged-psp
 kubectl get images.icp.ibm.com
